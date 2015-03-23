5b968e7222fe7ef790b948f0ad0dd34d0977ee5f
Tyler Erskine <tyler.w.erskine@gmail.com>
2015-03-23 01:50:50 -0400

    intitial commit

---
+++ HelloWorld/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HelloWorld::Application.config.secret_key_base = '87e30d69979da48f3fbcbc23e2c5f43013e036041c9b16e0e4231f4af620105bd0b27c250d7d12176db2129a3137315df51160191a7e0936fe1a9d6dc3dd116d'
--- config.ru
+++
@@ -1,4 +0,0 @@
-# config.ru
-require File.join( File.dirname(__FILE__), 'app' )
-
-run MyWebApp
